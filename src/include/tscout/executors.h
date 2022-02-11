ed145ceda024e48afceb7c2614274f6d73ca00f9
Matt Butrovich <mbutrovich@gmail.com>
2022-02-10 20:13:50 -0500

    Use the TopMemoryContext.

--- src/include/tscout/executors.h
+++ src/include/tscout/executors.h
@@ -38,18 +38,17 @@ static int ChildPlanNodeId(const struct Plan *const child_plan_node) {
  * src/backend/executors/nodeHash.c
  * src/backend/executors/nodeHashjoin.c
  */
-#define TS_EXECUTOR_WRAPPER(node_type)                                   \
-  static TupleTableSlot *Exec##node_type(PlanState *pstate) {            \
-    if (tscout_executor_running) {                                       \
-      TupleTableSlot *result;                                            \
-      TS_MARKER(Exec##node_type##_begin, pstate->plan->plan_node_id,     \
-                MemoryContextMemAllocated(TopTransactionContext, true)); \
-                                                                         \
-      result = WrappedExec##node_type(pstate);                           \
-                                                                         \
-      TS_MARKER(Exec##node_type##_end, pstate->plan->plan_node_id,       \
-                MemoryContextMemAllocated(TopTransactionContext, true)); \
-      return result;                                                     \
-    }                                                                    \
-    return WrappedExec##node_type(pstate);                               \
+#define TS_EXECUTOR_WRAPPER(node_type)                                                                                 \
+  static TupleTableSlot *Exec##node_type(PlanState *pstate) {                                                          \
+    if (tscout_executor_running) {                                                                                     \
+      TupleTableSlot *result;                                                                                          \
+      TS_MARKER(Exec##node_type##_begin, pstate->plan->plan_node_id,                                                   \
+                MemoryContextMemAllocated(TopMemoryContext, true));                                                    \
+                                                                                                                       \
+      result = WrappedExec##node_type(pstate);                                                                         \
+                                                                                                                       \
+      TS_MARKER(Exec##node_type##_end, pstate->plan->plan_node_id, MemoryContextMemAllocated(TopMemoryContext, true)); \
+      return result;                                                                                                   \
+    }                                                                                                                  \
+    return WrappedExec##node_type(pstate);                                                                             \
   }
